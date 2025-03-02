5917fefe7ff398a7e992908aa02d2f92888bcb4d
OmarGhoche <omarghoucher@gmail.com>
2025-03-02 16:31:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,25 +9,25 @@ $(document).ready(function () {
     });
 
     $('form').on('submit', (e) => {
-            e.preventDefault();
+        e.preventDefault();
 
-            const urlNewImg = $('#url-new-img').val();
-            const newLi= $('<li style="display: none"></li>');
-            
-            $(`<img src="${urlNewImg}" />`).appendTo(newLi);
-            $(`
+        const urlNewImg = $('#url-new-img').val();
+        const newLi = $('<li style="display: none"></li>');
+
+        $(`<img src="${urlNewImg}" />`).appendTo(newLi);
+        $(`
                 <div class="overlay-img-link">
                     <a href="${urlNewImg}" target="_blank" title="Ver imagem em tamanho real">
                         Ver imagem em tamanho real
                     </a>
                 </div>            
             `).appendTo(newLi);
-            $(newLi).appendTo('ul');
-            $(newLi).fadeIn();
-            
-            $('#url-new-img').val('');
+        $(newLi).appendTo('ul');
+        $(newLi).fadeIn();
 
-        });
+        $('#url-new-img').val('');
+
+    });
 
 
 });
